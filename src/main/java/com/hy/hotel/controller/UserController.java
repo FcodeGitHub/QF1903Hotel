bcbdb1e744ec76a23f0ef23bca6f19823152fdfb
yangyang <1208488082@qq.com>
2019-07-19 20:15:43 +0800

    fyytest2

--- src/main/java/com/hy/hotel/controller/UserController.java
+++ src/main/java/com/hy/hotel/controller/UserController.java
@@ -21,6 +21,10 @@ public class UserController {
     @RequestMapping("/login")
     @ResponseBody
     public String login (UserLogin userLogin, HttpServletResponse response) {
+
+        //解决跨域问题
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
         // 登录
         boolean result = userService.login(userLogin);
         // 验证登录是否正确跳转页面
@@ -36,7 +40,11 @@ public class UserController {
      */
     @RequestMapping("/insertUser")
     @ResponseBody
-    public String insertUser(UserLogin userLogin) {
+    public String insertUser(UserLogin userLogin,HttpServletResponse response) {
+
+        //解决跨域问题
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
         boolean result = userService.insertUser(userLogin);
         if (result) {
             return "success";
@@ -50,7 +58,11 @@ public class UserController {
      */
     @RequestMapping("updatePassword")
     @ResponseBody
-    public String updatePassword(UserLogin userLogin) {
+    public String updatePassword(UserLogin userLogin,HttpServletResponse response) {
+
+        //解决跨域问题
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
         boolean result = userService.updatePassword(userLogin);
         if (result) {
             return "success";
