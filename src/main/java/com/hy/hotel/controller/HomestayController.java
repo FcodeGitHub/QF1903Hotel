01be3c14723d7ef72ad52112226457614cc06c53
dsq <739437747@qq.com>
2019-07-20 15:10:00 +0800

    dsptest13

--- src/main/java/com/hy/hotel/controller/HomestayController.java
+++ src/main/java/com/hy/hotel/controller/HomestayController.java
@@ -3,7 +3,6 @@ package com.hy.hotel.controller;
 import com.hy.hotel.pojo.Homestay;
 import com.hy.hotel.service.HomestayService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
